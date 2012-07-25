4a29efac74507cb83de70f33a0fd2f5dad67531c
Dheeraj Kumar Ketireddy <dheeru@agiliq.com>
2012-07-25 15:21:59 +0530

    Improved the First View

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,16 @@
+#from django.template import Context, loader
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from polls.models import Poll
 
 def index(request):
-    return HttpResponse("Hello, World. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    #t = loader.get_template('polls/index.html')
+    #c = Context({
+    #    'latest_poll_list': latest_poll_list,
+    #})
+    #return HttpResponse(t.render(c))
+    return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
     return HttpResponse("You're looking at poll %s." % poll_id)
