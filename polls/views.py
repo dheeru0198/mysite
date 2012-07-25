44d372cfeef2e68b46ed38f0f6c3e6cc17126365
Dheeraj Kumar Ketireddy <dheeru@agiliq.com>
2012-07-25 17:46:05 +0530

    Decoupled the urlconf of poll app

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,6 @@
 #from django.template import Context, loader
-from django.http import HttpResponse
-from django.shortcuts import render_to_response
+from django.http import HttpResponse, Http404
+from django.shortcuts import render_to_response, get_object_or_404
 from polls.models import Poll
 
 def index(request):
@@ -13,7 +13,13 @@ def index(request):
     return render_to_response('polls/index.html', {'latest_poll_list': latest_poll_list})
 
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll %s." % poll_id)
+    #try:
+    #    p = Poll.objects.get(pk=poll_id)
+    #except Poll.DoesNotExist:
+    #    raise Http404
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render_to_response('polls/detail.html', {'poll': p})
+    #return HttpResponse("You're looking at poll %s." % poll_id)
 
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll %s." % poll_id)
